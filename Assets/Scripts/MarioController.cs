efc97c8d49d02cd77decf4facfea92f50fafc517
Osman S <osrbck@gmail.com>
2023-10-05 23:38:42 +0300

    Mario Controller and Some Level-1 designs

--- Assets/Scripts/MarioController.cs
+++ Assets/Scripts/MarioController.cs
@@ -4,28 +4,65 @@ using UnityEngine;
 
 public class MarioController : MonoBehaviour
 {
+    private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
+    private Collider2D _col;
     private Vector2 _direction;
 
     [SerializeField] private float _speed;
+    [SerializeField] private float _jump;
+    [SerializeField] private bool _isGround;
+    [SerializeField] private Transform _gizmo;
 
 
     void Start()
     {
         if(_rigidbody == null)
             _rigidbody = GetComponent<Rigidbody2D>();
+
+        if (_col == null)
+            _col= GetComponent<Collider2D>();
+
+        if (_spriteRenderer == null)
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(_gizmo == null)
+            _gizmo = transform.Find("Gizmo");
     }
 
-    void Update()
-    {   
-     _direction.x = Input.GetAxis("Horizontal") * _speed;
-     _direction.y = Input.GetAxis("Vertical") * _speed;
+    private void CheckCollision()
+    {
+
     }
-
-
     void FixedUpdate()
     {
-        _rigidbody.MovePosition(_rigidbody.position + _direction * Time.fixedDeltaTime);
+        CheckCollision();
+
+        int layerMask = LayerMask.GetMask("Ground");
+
+        _isGround = Physics2D.OverlapPoint(_gizmo.position, layerMask);
+        float moveX = Input.GetAxis("Horizontal");
+
+        if (moveX < 0f)
+            _spriteRenderer.flipX = true;
+
+        else
+            _spriteRenderer.flipX = false;
+
+        if ( _isGround && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
+        {
+            _rigidbody.AddForce(Vector2.up * _jump * Time.fixedDeltaTime, ForceMode2D.Impulse);
+
+
+            if (_direction.x > 0f)
+                transform.eulerAngles = Vector3.zero;
+            else if (_direction.x < 0f)
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        }
+
+        Vector2 newVelocity = new Vector2(moveX * _speed * Time.fixedDeltaTime, _rigidbody.velocity.y);
+
+        _rigidbody.velocity = newVelocity;
     }
 
 }
