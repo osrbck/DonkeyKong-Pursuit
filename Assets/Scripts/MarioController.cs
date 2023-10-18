a769dd4a9c86b0f4ec3eb38f72d38632dba32f72
Osman S <osrbck@gmail.com>
2023-10-18 05:10:56 +0300

    Mario Controller updated

--- Assets/Scripts/MarioController.cs
+++ Assets/Scripts/MarioController.cs
@@ -7,23 +7,43 @@ namespace DonkeyKongPursuit
 {
     public class MarioController : MonoBehaviour
     {
-        private MarioData _marioData;
-        
-        private MarioAgent _marioAgent;
         private PauseMenu _pauseMenu;
 
-        
+        private MarioData _marioData;
+        public MarioData MarioData { get { return _marioData; } }
+
+        [SerializeField] private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Sprite[] _runSprites;
+        [SerializeField] private Sprite _climbSprite;
+        private int spriteID;
+
+        [SerializeField] private bool _isGround;
+        [SerializeField] private bool _isClimbing;
+
+        private Vector2 _direction;
         private Rigidbody2D _rigidbody;
         private Collider2D _col;
-        private Collider2D[] _overlaps = new Collider2D[4];
-        private Vector2 _direction;
+        private Collider2D[] _colOverlaps;
 
-        [SerializeField] private bool _isClimbing;
-        [SerializeField] private bool _isGround;
+        private void OnEnable()
+        {
+            InvokeRepeating(nameof(AnimateMario), 1f / 12f, 1f / 12f);
+        }
 
-
-        public MarioData MarioData { get { return _marioData; } }
-        public bool IsClimbing { get { return _isClimbing; } }
+        private void OnDisable()
+        {
+            CancelInvoke();
+        }
+        private void Awake()
+        {
+            if (_spriteRenderer == null)
+                _spriteRenderer = GetComponent<SpriteRenderer>();
+            if (_climbSprite == null)
+                _climbSprite = GetComponent<Sprite>();
+            if (_runSprites == null)
+                _runSprites = GetComponent<Sprite[]>();
+            _marioData = GetComponent<MarioData>();
+        }
 
         void Start()
         {
@@ -31,88 +51,83 @@ namespace DonkeyKongPursuit
                 _rigidbody = GetComponent<Rigidbody2D>();
             if (_col == null)
                 _col = GetComponent<Collider2D>();
-            if (_overlaps == null)
-                _overlaps = GetComponent<Collider2D[]>();
-
-            _marioData = GetComponent<MarioData>();
-            _marioAgent= GetComponent<MarioAgent>();
+            _colOverlaps = new Collider2D[4];
         }
 
-        //private void OnEnable()
-        //{
-        //    InvokeRepeating(nameof(AnimateMario), 1f / 12f, 1f / 12f);
-        //}
-
-        //private void OnDisable()
-        //{
-        //    CancelInvoke();
-        //}
-
+        
         private void Update()
         {
+            
             if (Input.GetKeyDown(KeyCode.Escape))
             {
                 MenuManager.GoToMenu(MenuName.Pause);
             }
-                
+
         }
 
-        void FixedUpdate()
+        private void FixedUpdate()
         {
             CheckCollision();
             SetDirection();
             _rigidbody.MovePosition(_rigidbody.position + _direction * Time.fixedDeltaTime);
-
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
         {
+
             if (collision.collider.tag == "Danger")
             {
                 enabled = false;
                 GameManager._instance.OnLevelFailed();
             }
-            else if(collision.collider.tag == "Princess")
+            else if (collision.collider.tag == "Princess")
             {
                 enabled = false;
                 GameManager._instance.OnLevelCompleted();
             }
         }
 
+
         private void SetDirection()
         {
             if (_isClimbing)
             {
-                _direction.y = Input.GetAxis("Vertical") * _marioData.ClimbSpeed * Time.deltaTime;
+                _direction.y = Input.GetAxis("Vertical") * _marioData.ClimbSpeed * Time.fixedDeltaTime;
                 _rigidbody.gravityScale = 0f;
-                _marioAgent.ClimbingAnimate(_direction);
             }
 
             else if (_isGround && (Input.GetButtonDown("Jump") || Input.GetKey(KeyCode.W)))
             {
-                _direction = Vector2.up * _marioData.JumpSpeed * Time.deltaTime;
+                _direction = Vector2.up * _marioData.JumpSpeed * Time.fixedDeltaTime;
             }
 
             else
             {
-                _direction += Physics2D.gravity * Time.deltaTime;
+                _direction += Physics2D.gravity * Time.fixedDeltaTime;
             }
 
-            _direction.x = Input.GetAxis("Horizontal") * _marioData.MoveSpeed * Time.deltaTime;
-            if(_isGround)
-                _marioAgent.RunningAnimate(_direction);
+            _direction.x = Input.GetAxis("Horizontal") * _marioData.MoveSpeed * Time.fixedDeltaTime;
 
             // Prevent gravity from building up infinitely
             if (_isGround)
             {
                 _direction.y = Mathf.Max(_direction.y, -1f);
             }
+
+            if (_direction.x > 0f)
+            {
+                transform.eulerAngles = Vector3.zero;
+            }
+            else if (_direction.x < 0f)
+            {
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
         }
 
-        void CheckCollision()
+        private void CheckCollision()
         {
             _isGround = false;
-            _isClimbing = false;
+            _isClimbing= false;
 
             // the amount that two colliders can overlap
             // increase this value for steeper platforms
@@ -120,19 +135,19 @@ namespace DonkeyKongPursuit
             Vector2 size = _col.bounds.size;
             size.y += skinWidth;
             size.x /= 1.5f;
-            int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, _overlaps);
+            int amount = Physics2D.OverlapBoxNonAlloc(transform.position, size, 0f, _colOverlaps);
 
             for (int i = 0; i < amount; i++)
             {
-                GameObject hit = _overlaps[i].gameObject;
+                GameObject hit = _colOverlaps[i].gameObject;
 
-                if (hit.layer == LayerMask.NameToLayer("Ground"))
+                if (hit.layer == LayerMask.NameToLayer("Platform"))
                 {
                     // Only set as grounded if the platform is below the player
                     _isGround = hit.transform.position.y < (transform.position.y - 0.5f + skinWidth);
 
                     // Turn off collision on platforms the player is not grounded to
-                    Physics2D.IgnoreCollision(_overlaps[i], _col, !_isGround);
+                    Physics2D.IgnoreCollision(_colOverlaps[i], _col, !_isGround);
                 }
                 else if (hit.layer == LayerMask.NameToLayer("Ladder"))
                 {
@@ -141,30 +156,22 @@ namespace DonkeyKongPursuit
             }
         }
 
-        //public void AnimateMario()
-        //{
-        //    if (_direction.x < 0f)
-        //        _spriteRenderer.flipX = true;
+        public void AnimateMario()
+        {
+            if (_isClimbing && !_isGround)
+            {
+               _spriteRenderer.sprite = _climbSprite;       
+            }
 
-        //    else
-        //        _spriteRenderer.flipX = false;
+            else if (_direction.x != 0f && _isGround)
+            {
+                spriteID++;
+                if (spriteID >= _runSprites.Length)
+                    spriteID = 0;
+                _spriteRenderer.sprite = _runSprites[spriteID];
+            }
 
-        //    if (_direction.x != 0f)
-        //        _animator.SetBool("isRunning", true);
-
-        //    else
-        //        _animator.SetBool("isRunning", false);
-
-        //    if (_direction.y != 0f)
-        //        _animator.SetBool("isClimbing", true);
-
-        //    else
-        //    {
-        //        _animator.SetBool("isClimbing", false);
-        //        _animator.SetTrigger("Climb");
-        //    }
-
-        //}
+        }
 
     }
 }
